0b13464a5c1d6c7f855911d93b5e1a81d930ffc1
Kondratenko Pavel <p.kondratenko@rambler-co.ru>
2017-05-25 16:07:48 +0300

    replace cont with let

--- app/components/cookies.js
+++ app/components/cookies.js
@@ -16,8 +16,8 @@ export default class extends Base {
   get(name) {
     const nameEQ = `${name}=`;
     const ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-      var c = ca[i];
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i];
       while (c.charAt(0) === ' ') c = c.substring(1, c.length);
       if (c.indexOf(nameEQ) === 0) {
         return c.substring(nameEQ.length, c.length);
@@ -29,8 +29,8 @@ export default class extends Base {
 
   isset(name) {
     const ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-      var c = ca[i];
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i];
       while (c.charAt(0) === ' ') c = c.substring(1, c.length);
       if (c.indexOf(name) > -1) {
         return true;
